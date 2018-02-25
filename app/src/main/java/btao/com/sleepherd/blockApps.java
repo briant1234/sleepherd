8a8a4a37b7ad9f1a1a29d4c6b6ec3b675a5a7124
briant1234 <btao.person@gmail.com>
2018-02-25 12:25:55 -0500

    really basic layouts

---
+++ app/src/main/java/btao/com/sleepherd/blockApps.java
@@ -0,0 +1,13 @@
+package btao.com.sleepherd;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class blockApps extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_block_apps);
+    }
+}
